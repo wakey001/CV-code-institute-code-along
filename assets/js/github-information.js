0539b40476de055bfeeaf31e40a102398575d2c9
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-05-17 20:55:09 +0000

    add promise code

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -8,6 +8,27 @@ function fetchGitHubInformation(event) {
 
     $("#gh-user-data").html(
         `<div id="loader">
-            <img src="/assets/css/loader.gif" alt="loading..." />
+            <img src="/   assets/css/loader.gif" alt="loading..." />
         </div>`);
+
+    $.when(
+        $.getJSON('https://api.github.com/users/${username}')
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        }, function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorRespones);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message</h2>`);
+            }   
+                    
+
+        
+        });
+        
 }
