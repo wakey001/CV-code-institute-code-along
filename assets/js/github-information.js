4b96c4a3b0a74df1b3c521ede5b6a286fa767860
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-05-18 20:01:54 +0000

    pasted in code due to bad syntax

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -8,27 +8,24 @@ function fetchGitHubInformation(event) {
 
     $("#gh-user-data").html(
         `<div id="loader">
-            <img src="/   assets/css/loader.gif" alt="loading..." />
+            <img src="/assets/css/loader.gif" alt="loading..." />
         </div>`);
 
     $.when(
-        $.getJSON('https://api.github.com/users/${username}')
+        $.getJSON(`https://api.github.com/users/${username}`)
     ).then(
         function(response) {
             var userData = response;
             $("#gh-user-data").html(userInformationHTML(userData));
-        }, function(errorResponse) {
+        },
+        function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
             } else {
-                console.log(errorRespones);
+                console.log(errorResponse);
                 $("#gh-user-data").html(
-                    `<h2>Error: ${errorResponse.responseJSON.message</h2>`);
-            }   
-                    
-
-        
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
         });
-        
 }
